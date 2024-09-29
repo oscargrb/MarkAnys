5e52b6f4cf35e1410adaf892055a5fea455b2007
oscargrb <og.rondon@gmail.com>
2024-09-29 12:29:55 -0400

    saves

--- Config/AllowedOrigins.js
+++ Config/AllowedOrigins.js
@@ -1,10 +1,11 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:5500',
-    'http://localhost:4173',
-    'http://localhost:1420'
+  "https://www.yoursite.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+  "http://localhost:5500",
+  "http://localhost:4173",
+  "http://localhost:1420",
+  "http://192.168.1.111:8081",
 ];
 
 module.exports = allowedOrigins;
--- Config/CorsOptions.js
+++ Config/CorsOptions.js
@@ -1,15 +1,15 @@
-const allowedOrigins = require('./AllowedOrigins');
+const allowedOrigins = require("./AllowedOrigins");
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        console.log(origin)
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
-}
+  origin: (origin, callback) => {
+    ad;
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
 
 module.exports = corsOptions;
